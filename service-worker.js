de13e25a8509568b65daca6d9e547dea15d991eb
loweliev <loweren14@gmail.com>
2025-07-21 14:41:38 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,21 @@
 const CACHE_NAME = "agenda-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png",
   "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
 ];
 
-// Al instalar el SW, cacheamos archivos esenciales
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Al hacer fetch, servimos desde cache si está disponible
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
@@ -27,7 +24,6 @@ self.addEventListener("fetch", (event) => {
   );
 });
 
-// Al activar, limpiamos caches viejos si existen
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) =>
