8f8c9870073075ecc2a09212c0240ee6f6d24d11
loweliev <loweren14@gmail.com>
2025-07-21 22:35:21 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,24 @@
-
-// Importa Firebase y configura tu base de datos
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getDatabase, ref, get, set, push, update, remove, onValue } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  get,
+  set,
+  push,
+  update,
+  remove,
+  onValue
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD_TH5CFzXrAHWUffsjy6IHe5AexSw1fcg",
   authDomain: "agenda-mya.firebaseapp.com",
+  databaseURL: "https://agenda-mya-default-rtdb.firebaseio.com",
   projectId: "agenda-mya",
   storageBucket: "agenda-mya.appspot.com",
   messagingSenderId: "640452417589",
-  appId: "1:640452417589:web:10135d2a635afb1713397c",
-  databaseURL: "https://agenda-mya-default-rtdb.firebaseio.com"
+  appId: "1:640452417589:web:10135d2a635afb1713397c"
 };
 
 const app = initializeApp(firebaseConfig);
